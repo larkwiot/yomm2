206bad0089be5e1fc191ab90f6eff9c5a06f9c57
Jean-Louis Leroy <jl@leroy.nyc>
2023-08-13 12:40:16 -0400

    rework trace

--- examples/synopsis.cpp
+++ examples/synopsis.cpp
@@ -81,7 +81,6 @@ define_method(void, meet, (Cat& cat, Dog& dog, std::ostream& os)) {
 #include <string>
 
 int main() {
-    yorel::yomm2::default_policy::runtime_trace::os = &std::cout;
     // Initialise method dispatch tables.
     yorel::yomm2::update();
 
